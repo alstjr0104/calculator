7de2d45452b46f6a7f2f981b27a075fcd016c522
kihwankim-nhn <kihwankim.nhn@gmail.com>
2022-01-24 17:46:28 +0900

    comment 변경

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ int main(int argc, char* argv[])
 	int number1, number2, result;
 	char operator;
 	
-	printf("Please enter a expression: ");
+	printf("수식 입력 필요: ");
 	scanf("%d %c %d", &number1, &operator, &number2);
 	
 	switch (operator)
