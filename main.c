efbd9d9ef2e3f670d9cc364c3abb9b65b6c664df
alstjr0104 <alstjr0104@gmail.com>
2022-01-24 16:54:21 +0900

    #인프라기술팀-Git-교육/1 공백 및 콜론 수정

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ int main(int argc, char* argv[])
 	int number1, number2, result;
 	char operator;
 	
-	printf("Please enter a expression");
+	printf("Please enter a expression: ");
 	scanf("%d %c %d", &number1, &operator, &number2);
 	
 	switch (operator)
