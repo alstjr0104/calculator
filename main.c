904b220f96fc680729a9a84225c955f12513942f
alstjr0104 <alstjr0104@gmail.com>
2022-01-24 16:46:00 +0900

    #인프라기술팀-Git-교육 변수가 의미있도록 변경

--- main.c
+++ main.c
@@ -7,29 +7,53 @@ int multi(int, int);
  
 int main(int argc, char* argv[])
 {
-	int a, b, c;
+	int number1, number2, result;
 	char operator;
 	
 	printf("Please enter a expression");
-	scanf("%d %c %d", &a, &operator, &b);
+	scanf("%d %c %d", &number1, &operator, &number2);
 	
 	switch (operator)
 	{
 		case '+':
-			c = add(a, b);
+			result = add(number1, number2);
 			break;
 		case '-':
-			c = sub(a, b);
+			result = sub(number1, number2);
 			break;
 		case '*':
-			c = multi(a, b);
+			result = multi(number1, number2);
 			break;
 		case '/':
-			c = div(a, b);
+			result = div(number1, number2);
 			break;
 	}
 	
-	printf("result is %d.\n", c);
+	printf("result is %d.\n", result);
 	
+	return 0;
+}
+ 
+int add(int number1, int number2)
+{
+	//TODO implementation
+	return 0;
+}
+ 
+int sub(int number1, int number2)
+{
+	//TODO implementation
+	return 0;
+}
+ 
+int multi(int number1, int number2)
+{
+	//TODO implementation
+	return 0;
+}
+ 
+int div(int number1, int number2)
+{
+	//TODO implementation
 	return 0;
 }
